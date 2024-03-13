2f49ab63fdcd0b98c27fcab0dbe8957bac8c478b
Yarmak Dmytro <65123338+Yarmacho@users.noreply.github.com>
2024-03-13 21:19:40 +0200

    Add files via upload

---
+++ EnumAttributes.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Common.Tools
+{
+    public class EnumAttributes
+    {
+        public static Dictionary<Type, List<KeyValuePair<string, MapValueAttribute>>> _enumCache = new Dictionary<Type, List<KeyValuePair<string, MapValueAttribute>>>();
+
+        private List<KeyValuePair<string, MapValueAttribute>> _enumValues;
+        private EnumAttributes(Type enumType)
+        {
+            if (!_enumCache.TryGetValue(enumType, out List<KeyValuePair<string, MapValueAttribute>> enumValues))
+            {
+                enumValues = new List<KeyValuePair<string, MapValueAttribute>>();
+                foreach (var value in enumType.GetFields(BindingFlags.Public | BindingFlags.Static))
+                {
+                    var mapValue = value.GetCustomAttributes<MapValueAttribute>().FirstOrDefault();
+                    if (mapValue != null)
+                    {
+                        enumValues.Add(new KeyValuePair<string, MapValueAttribute>(value.Name, mapValue));
+                    }
+                }
+                _enumCache.Add(enumType, enumValues);
+            }
+
+            _enumValues = enumValues;
+        }
+
+        public static T ValueToEnum<T>(object value) where T : Enum
+        {
+            return (T)ValueToEnum(value, typeof(T));
+        }
+
+        internal static object ValueToEnum(object value, Type enumType)
+        {
+            var attributes = new EnumAttributes(enumType);
+            return attributes.valueToEnum(value, enumType);
+        }
+
+        private string getEnumField(object value)
+        {
+            foreach (var enumValue in _enumValues)
+            {
+                if (enumValue.Key.Equals(value) || enumValue.Value.SqlValue.Equals(value) ||
+                    (enumValue.Value.AltSqlValue != null && enumValue.Value.AltSqlValue.Equals(value)))
+                {
+                    return enumValue.Key;
+                }
+            }
+
+            throw new InvalidEnumArgumentException($"Enum member {value} is missing");
+        }
+
+        private object valueToEnum(object value, Type enumType)
+        {
+            return Enum.Parse(enumType, getEnumField(value));
+        }
+
+        public static object EnumToValue<T>(Enum member)
+        {
+            return EnumToValue(member, member.GetType());
+        }
+
+        public static object EnumToValue(Enum member, Type enumType)
+        {
+            var attributes = new EnumAttributes(enumType);
+            return attributes.enumToValue(member);
+        }
+
+        public static string GetEnumCaption(Enum member, Type enumType)
+        {
+            var attributes = new EnumAttributes(enumType);
+            return attributes.getEnumCaption(member);
+        }
+
+        public static string GetEnumCaption<T>(Enum member)
+        {
+            return GetEnumCaption(member, typeof(T));
+        }
+
+        private object enumToValue(Enum member)
+        {
+            var stringValue = member.ToString();
+            foreach (var enumValue in _enumValues)
+            {
+                if (enumValue.Key.Equals(stringValue))
+                {
+                    return enumValue.Value.SqlValue;
+                }
+            }
+
+            return stringValue;
+        }
+        
+        private string getEnumCaption(Enum member)
+        {
+            var stringValue = member.ToString();
+            foreach (var enumValue in _enumValues)
+            {
+                if (enumValue.Key.Equals(stringValue))
+                {
+                    return string.IsNullOrEmpty(enumValue.Value.Caption)
+                        ? enumValue.Value.SqlValue.ToString()
+                        : enumValue.Value.Caption;
+                }
+            }
+
+            return stringValue;
+        }
+    }
+}
---
+++ TypeTools/TypeTools.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Common.Tools
+{
+    public static class TypeTools
+    {
+        private static Dictionary<Type, List<MethodInfo>> _operatorsCache = new Dictionary<Type, List<MethodInfo>>();
+
+        public static T Convert<T>(object value)
+        {
+            return (T)convert(value, typeof(T));
+        }
+
+        private static object convert(object value, Type targetType)
+        {
+            if (value is null || value == DBNull.Value)
+            {
+                return targetType.IsValueType ? Activator.CreateInstance(targetType) : default;
+            }
+
+            var valueType = value.GetType();
+            if (valueType == targetType || valueType.IsInstanceOfType(targetType))
+            {
+                return value;
+            }
+
+            if (targetType == typeof(string))
+            {
+                return value.ToString();
+            }
+
+            if (targetType.IsGenericType)
+            {
+                var underlyingType = Nullable.GetUnderlyingType(targetType);
+                if (underlyingType != null)
+                {
+                    return Activator.CreateInstance(targetType, convert(value, underlyingType));
+                }
+            }
+
+            if (valueType.IsEnum)
+            {
+                return EnumAttributes.EnumToValue((Enum)value, targetType);
+            }
+
+            if (targetType.IsEnum)
+            {
+                return EnumAttributes.ValueToEnum(value, targetType);
+            }
+
+            try
+            {
+                return System.Convert.ChangeType(value, targetType);
+            }
+            catch
+            {
+                if (!_operatorsCache.TryGetValue(targetType, out List<MethodInfo> operators))
+                {
+                    operators = new List<MethodInfo>();
+                    foreach (var method in targetType.GetMethods(BindingFlags.Public | BindingFlags.Static))
+                    {
+                        if (method.Name.StartsWith("op_Explicit") || method.Name.StartsWith("op_Implicit"))
+                        {
+                            operators.Add(method);
+                        }
+                    }
+                    _operatorsCache.Add(targetType, operators);
+                }
+
+                foreach (var oper in operators)
+                {
+                    if (oper.ReturnType == targetType)
+                    {
+                        return oper.Invoke(null, new object[] { value });
+                    }
+                }
+            }
+
+            return targetType.IsValueType ? Activator.CreateInstance(targetType) : null;
+        }
+    }
+}
